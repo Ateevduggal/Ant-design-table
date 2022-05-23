439c0f48c7aad619cf0c54d26932ecf1e73a7b1c
AteevDuggal <akd3257@gmail.com>
2022-05-23 11:35:28 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Table, Modal } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { data } from "./Data";
+import Modals from "./Modals";
 
-function App() {
+const App = () => {
+  const columns = [
+    {
+      key: "name",
+      title: "Name",
+      dataIndex: "name",
+      sorter: (a, b) => a.name > b.name,
+      sortDirections: ["descend"],
+    },
+    {
+      key: "email",
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      key: "address",
+      title: "Address",
+      dataIndex: "address",
+      filters: [
+        {
+          text: "London",
+          value: "London",
+        },
+        {
+          text: "New York",
+          value: "New York",
+        },
+      ],
+      onFilter: (value, record) => record.address.indexOf(value) === 0,
+    },
+    {
+      key: "phone",
+      title: "Phone Number",
+      dataIndex: "phone",
+    },
+    {
+      key: "website",
+      title: "Website",
+      dataIndex: "website",
+      // render: (website) => <a href={website}>{website}</a>,
+    },
+    {
+      key: "action",
+      title: "Actions",
+      render: (record) => {
+        return (
+          <>
+            <div className="flex">
+              <EditOutlined
+                style={{ color: "black" }}
+                onClick={() => Edit(record)}
+              />
+              <DeleteOutlined
+                style={{ color: "red" }}
+                onClick={() => Delete(record)}
+              />
+            </div>
+          </>
+        );
+      },
+    },
+  ];
+
+  const [Data, setData] = useState(data);
+  const [visible, setVisible] = useState(false);
+  const [edit, setEdit] = useState(null);
+
+  const Delete = (record) => {
+    Modal.confirm({
+      title: "Are you sure you want to delete this",
+      onOk: () => {
+        setData((pre) => {
+          return pre.filter((person) => person.id != record.id);
+        });
+      },
+    });
+  };
+
+  const Edit = (record) => {
+    setVisible(true);
+    setEdit({ ...record });
+  };
+
+  const ResetEditing = () => {
+    setVisible(false);
+    setEdit(true);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <div className="table">
+          <Table
+            dataSource={Data}
+            columns={columns}
+            pagination={{ pageSize: 8, total: 50, showSizeChanger: true }}
+            bordered
+          />
+          <Modals
+            visible={visible}
+            edit={edit}
+            setEdit={setEdit}
+            setData={setData}
+            ResetEditing={ResetEditing}
+            setVisible={setVisible}
+          />
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
